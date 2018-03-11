ba4b96dc78cb0c7854a428c2b5b6ddbb3fb91492
manolis <manolisan28@gmail.com>
2018-03-11 03:06:07 +0200

    push origin master

--- client.py
+++ client.py
@@ -1,50 +1,20 @@
 import zmq
 import sys
 import socket
-import struct
-
-
-if (len(sys.argv) != 3):
-    print "Usage: python client.py [port] [username]"
-    exit()
-
-host = 'localhost'
-port = sys.argv[1]
-username = sys.argv[2]
-
-my_id = 0
-context = zmq.Context()
-
-# # Initialize soccents and connect to tracker
-print("Connecting to " + host + " server")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://" + host  + ":5555")
-
-## REGISTER to thhe service
-print ("------------------------")
-print("Sending register request [ !r ]")
-socket.send(b"!r %s %s %s" %(host, port, username))
-
-message = socket.recv()
-if (message.startswith('*')):
-    print ("Error: ", message.lstrip('*'))
-else:
-    my_id = message
-    print ("REPLY --> ID: %s" %my_id)
-
+import select
 
 # client give commands from stdin
-while True:
+def send_input():
     print ("------------------------")
-    line = raw_input("[%s]>" %username)
+    line = sys.stdin.readline()
     user_input = line.rstrip("\n ")
     user_input = ' '.join(user_input.split())
 
     # input command
     if (user_input.startswith('!')):
         print("REQUEST: [ %s ]" %user_input)
-        socket.send(user_input + " " + my_id)
-        message = socket.recv()
+        socket_tcp.send(user_input + " " + my_id)
+        message = socket_tcp.recv()
         if (message.startswith('*')):
             print "Error: ", message.lstrip('*')
         else:
@@ -53,3 +23,60 @@ while True:
     else:
         print "TODO: Sending user input! "
         #sender_sock.send(user_input)
+
+    sys.stdout.write("[%s]>" %username); sys.stdout.flush()
+
+
+if (len(sys.argv) != 3):
+    print "Usage: python client.py [port] [username]"
+    exit()
+
+host = "localhost"
+port = int(sys.argv[1])
+username = sys.argv[2]
+
+my_id = 0
+context = zmq.Context()
+
+# # Initialize soccents and connect to tracker
+print("Connecting to " + host + " server")
+socket_tcp = context.socket(zmq.REQ)
+socket_tcp.connect("tcp://" + host  + ":5555")
+
+## REGISTER to thhe service
+print ("------------------------")
+print("Sending register request [ !r ]")
+socket_tcp.send(b"!r %s %s %s" %(host, port, username))
+
+message = socket_tcp.recv()
+if (message.startswith('*')):
+    print ("Error: ", message.lstrip('*'))
+else:
+    my_id = message
+    print ("REPLY --> ID: %s" %my_id)
+
+## udp sockets.
+receive_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+receive_sock.bind((host, port))
+
+sys.stdout.write("[%s]>" %username); sys.stdout.flush()
+
+while True:
+    readable, writable, exceptional = select.select([sys.stdin, receive_sock], [], [])
+    for s in readable:
+        if receive_sock == s:
+            # incoming message from peer
+            data = receive_sock.recv(1024)
+            if not data:
+                print "Error receiving data"
+                sys.exit()
+            else:
+                sys.stdout.write('\n')
+                sys.stdout.write(data)
+                sys.stdout.write('\n')
+                sys.stdout.write("[%s]>" %username); sys.stdout.flush()
+
+        else:
+            #users writes message
+            send_input()
+            #sys.stdout.wr
